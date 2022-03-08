81f2aeb931505285f0743e0b346481144c3aeaf5
rodrigospicker1 <79051753+rodrigospicker1@users.noreply.github.com>
2022-03-08 08:44:53 -0300

    Add files via upload

---
+++ CursoWindowsForms/Frm_DemonstracaoKey.cs
@@ -0,0 +1,33 @@
+namespace CursoWindowsForms
+{
+    public partial class Frm_DemonstracaoKey : Form
+    {
+        public Frm_DemonstracaoKey()
+        {
+            InitializeComponent();
+        }
+
+        private void Txt_Input_KeyDown(object sender, KeyEventArgs e)
+        {
+            Txt_Msg.AppendText("\r\n" + "Pressionei uma tecla: " + e.KeyCode + "\r\n");
+            Txt_Msg.AppendText("\t" + "Código da tecla: " + ((int)e.KeyCode) + "\r\n");
+            Txt_Msg.AppendText("\t" + "Nome da tecla: " + e.KeyData + "\r\n");
+            Lbl_Lower.Text = e.KeyCode.ToString().ToLower();
+            Lbl_Upper.Text = e.KeyCode.ToString().ToUpper();
+        }
+
+        private void Btn_Reset_Click(object sender, EventArgs e)
+        {
+            Txt_Msg.Text = "";
+            Txt_Input.Text = "";
+            Lbl_Upper.Text = "";
+            Lbl_Lower.Text = "";
+
+        }
+
+        private void Frm_DemonstracaoKey_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
